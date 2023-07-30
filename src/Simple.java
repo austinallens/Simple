f6d48229559dff6418568b04a52ee8d64c1072de
Austin Allen <95332894+austinallens@users.noreply.github.com>
2023-07-30 01:31:34 -0500

    Add files via upload

---
+++ src/Simple.java
@@ -0,0 +1,21 @@
+/* Name: Austin Allen
+ * Course: CSC 135
+ * Project: Problem 1.2
+ * File Name: Simple.java
+ */
+
+public class Simple {
+
+    public static void main(String[] args) {
+        System.out.println("1. simple, the first letter of Java type should be "
+                + "in upper case.\n" 
+                + "2. SimpleProgram, is a valid Java identifier\n" 
+                + "3. 1 Simple, identifiers can't begin with a digit\n" 
+                + "4. _Simple_, identifiers start with a capital letter\n" 
+                + "5. *Simple*, identifiers can't use the \"*\" character\n" 
+                + "6. $123_45, \"$\" should only be used in machine-made code\n" 
+                + "7. Simple!, identifiers can't use the \"!\" character"
+        );
+    }
+    
+}
